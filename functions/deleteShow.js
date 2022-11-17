8d6cdee3690d3de289154f0dd45c3d665cee975f
= <tmelo387@gmail.com>
2022-11-17 15:11:24 -0300

    Feat: Adding delete function

--- functions/deleteShow.js
+++ functions/deleteShow.js
@@ -4,49 +4,23 @@ const expressionGenerator = require('../expressionGenerator');
 
 
 module.exports.handle = async (event) => {
-    const body = JSON.parse(event.body)
     const id = event.pathParameters.id
     const dynamoDB = new AWS.DynamoDB.DocumentClient()
     
-    const requiredFields = ['name', 'showDate']
-    const showDate = new Date(body.showDate)
-    const currentDate = new Date()
-    var expressions = expressionGenerator(body, showDate)
+ 
 
     try {
-
-        if (showDate == 'Invalid Date' || showDate.getTime() < currentDate.getTime()) {
-            throw new Error('Invalid Date')
-        }
-
-        for (let index = 0; index < requiredFields.length; index++) {
-            if (body[requiredFields[index]] == '' || body[requiredFields[index]] == undefined) {
-                throw new Error(`Required field ${requiredFields[index]} was not filled`)
-            }
-        }
-
         const putParams = {
             TableName: process.env.DYNAMODB_SHOW_TABLE,
             Key: {
                 primary_key: id,
-            },
-            UpdateExpression: "set #oldName = :newName, #oldDescription = :newDescription, #oldShowDate = :newShowDate",
-            ExpressionAttributeNames: {
-                "#oldName": "name",
-                "#oldDescription": "description",
-                "#oldShowDate": "showDate"
-            },
-            ExpressionAttributeValues: {
-                ":newName": body.name,
-                ":newDescription": body.description,
-                ":newShowDate": showDate
             }
         };
-        await dynamoDB.update(putParams).promise();
+        await dynamoDB.delete(putParams).promise();
 
         return {
             statusCode: 201,
-            body: JSON.stringify({ putParams, expressions})
+            body: JSON.stringify({message: "Item deleted successfully", putParams})
         };
 
     } catch (error) {
